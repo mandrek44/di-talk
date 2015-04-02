b122ee71f10fe36a686c58c0c65d088598906b58
Marcin Drobik <mdrobik@future-processing.com>
2015-04-02 09:45:05 +0200

    Converted to FullStack, single file, working app. Will used to refactor during workshops

---
+++ src/app/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.Owin.Hosting;
+
+using Owin;
+
+using ServiceStack;
+
+namespace Procent.DependencyInjection.app
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (WebApp.Start("http://+:12345", Startup))
+            {
+                Console.WriteLine("Listening at port 12345");
+                Console.ReadLine();
+            }
+        }
+
+        private static void Startup(IAppBuilder app)
+        {
+            var webServer = new WebServer();
+
+            app.Use(
+                async (context, next) =>
+                {
+                    Console.WriteLine(context.Request.Method + " " + context.Request.Path + context.Request.QueryString);
+                    try
+                    {
+                        await next();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.Error.WriteLine(e.ToString());
+                        throw;
+                    }
+                });
+
+            app.Use((context, next) =>
+            {
+                return Task.Run(() => webServer.RegisterUser(context.Request.Body.ReadLines().First()));
+            });
+        }
+    }
+}
--- src/app/WebServer.cs
+++ src/app/WebServer.cs
@@ -1,11 +1,58 @@
-﻿namespace Procent.DependencyInjection.app
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+using ServiceStack.OrmLite;
+
+namespace Procent.DependencyInjection.app
 {
     public class WebServer
     {
+        const string EMAIL_REGEX = @"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}";
+
         public void RegisterUser(string email)
         {
-            var controller = new UsersController();
-            controller.RegisterUser(email);
+            var connectionString = "local.db";
+            
+            OrmLiteConfig.DialectProvider = SqliteDialect.Provider;
+            var dbFactory = new OrmLiteConnectionFactory(connectionString, SqliteDialect.Provider);
+            var db = new OrmLiteConnection(dbFactory);
+            db.Open();
+
+            if (!db.TableExists<User>())
+                db.CreateTable<User>();
+
+            // check if email is valid
+            if (Regex.IsMatch(email, EMAIL_REGEX) == false)
+            {
+                throw new ArgumentException("Invalid email address");
+            }
+            
+            // check if email is not taken
+            if (db.Select<User>(u => u.Email == email).Any())
+            {
+                throw new InvalidOperationException("Email already taken");
+            }
+
+            // create new user
+            var newUser = new User
+            {
+                Email = email,
+                Name = Guid.NewGuid().ToString(),
+            };
+
+            // insert user
+            db.Insert(newUser);
+
+            // generate activation link
+            string registrationLink = string.Format("http://myapp.com/confirm?email={0}&token={1}", newUser.Email, newUser.Name);
         }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
     }
 }
